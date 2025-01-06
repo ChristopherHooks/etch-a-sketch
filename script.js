1371f5dc7e3ce12c474fbcabdb75baea44421306
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-06 14:15:21 -0600

    Configure working  etch-a-sketch

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('#container');
+
+function createBox() {
+    let div = document.createElement('div');
+    div.classList.add("box");
+    // div.textContent = 'div';
+    container.append(div);
+}
+
+for (let i = 0; i < 256; i++) {
+    createBox();
+}
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", (event) => {
+        box.style.backgroundColor = 'black';
+    }
+    )
+})
