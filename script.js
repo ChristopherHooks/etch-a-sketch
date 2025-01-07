895a108afc907007881e07184fd5f513b675a726
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-06 20:55:35 -0600

    Add incremental opacity

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function createBox() {
     div.style.height = "60px";
     div.style.width = "60px";
     div.classList.add("box");
+    div.style.opacity = 0;
     container.append(div);
 }
 
@@ -30,16 +31,28 @@ const boxes = document.querySelectorAll('.box');
 boxes.forEach((box) => {
     box.addEventListener("mouseover", (event) => {
         box.style.backgroundColor = 'black';
+        let opacity = parseFloat(box.style.opacity);
+        opacity += .1;
+        if (opacity > 1) {
+            opacity = 1;
+        }
+        box.style.opacity = opacity;
     }
     )
 })
-
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
     removeAllChildNodes(container);
 
+
     const numOfBoxes = prompt("Enter number of boxes");
+
+    if (numOfBoxes > 10000) {
+        alert("Boxes set to maximum 10,000");
+        numOfBoxes = 10000;
+    }
+
     const columns = Math.floor(Math.sqrt(numOfBoxes));
 
     const width = 960 / columns;
@@ -50,6 +63,7 @@ btn.addEventListener("click", () => {
         div.style.height = height + "px";
         div.style.width = width + "px";
         div.classList.add("box");
+        div.style.opacity = 0;
         container.append(div);
     }
 
@@ -57,6 +71,12 @@ btn.addEventListener("click", () => {
     boxes.forEach((box) => {
         box.addEventListener("mouseover", (event) => {
             box.style.backgroundColor = getRandomRGB();
+            let opacity = parseFloat(box.style.opacity);
+            opacity += .1;
+            if (opacity > 1) {
+                opacity = 1;
+            }
+            box.style.opacity = opacity;
         }
         )
     })
