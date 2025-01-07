4c58f3c75ee33da117485187f8443cb20204f79f
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-06 20:08:29 -0600

    Add custom grid functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('#container');
 
+
+
 function createBox() {
     let div = document.createElement('div');
+    div.style.height = "60px";
+    div.style.width = "60px";
     div.classList.add("box");
-    // div.textContent = 'div';
     container.append(div);
 }
 
@@ -12,7 +15,6 @@ for (let i = 0; i < 256; i++) {
 }
 
 const boxes = document.querySelectorAll('.box');
-
 boxes.forEach((box) => {
     box.addEventListener("mouseover", (event) => {
         box.style.backgroundColor = 'black';
@@ -20,7 +22,37 @@ boxes.forEach((box) => {
     )
 })
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
-    container.innerHTML = "";
+    removeAllChildNodes(container);
+
+    const numOfBoxes = prompt("Enter number of boxes");
+    const columns = Math.floor(Math.sqrt(numOfBoxes));
+
+    const width = 960 / columns;
+    const height = width;
+
+    for (i = 0; i < numOfBoxes; i++) {
+        let div = document.createElement('div');
+        div.style.height = height + "px";
+        div.style.width = width + "px";
+        div.classList.add("box");
+        container.append(div);
+    }
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", (event) => {
+            box.style.backgroundColor = 'black';
+        }
+        )
+    })
 })
+
+
